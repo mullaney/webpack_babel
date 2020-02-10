3bdd79396a65b48368dee05ce1c46cbe82c83282
Kevin Kelly <kvinklly@gmail.com>
2020-02-09 23:30:49 -0600

    add HtmlWebpackPlugin

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 console.log('1 + 1 =', 2);
+
+const h1 = document.createElement('h1');
+h1.innerHTML = "Webpack and Babel";
+
+document.body.appendChild(h1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,14 +9,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Webpack and Babel'
+  })],
   module: {
-    rules: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['@babel/preset-env']
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env']
+        }
       }
-    }]
+    ]
   }
 }
